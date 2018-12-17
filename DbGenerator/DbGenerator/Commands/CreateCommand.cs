2491ce012bcee633cbb7dae20a7bd64e35ee1848
Austin Lippert <6955342+aternex@users.noreply.github.com>
2018-12-17 16:59:12 -0500

    Use reference to cmsdata to create db

--- DbGenerator/DbGenerator/Commands/CreateCommand.cs
+++ DbGenerator/DbGenerator/Commands/CreateCommand.cs
@@ -1,10 +1,6 @@
-﻿using Consolas.Core;
+﻿using CmsData;
+using Consolas.Core;
 using DbGenerator.Args;
-using System;
-using System.Collections.Generic;
-using System.Data.SqlClient;
-using System.IO;
-using System.Text;
 
 namespace DbGenerator.Commands
 {
@@ -12,73 +8,14 @@ namespace DbGenerator.Commands
     {
         public object Execute(CreateArgs args)
         {
-            var cmsDbManifestPath = Path.Combine(args.ScriptDirectory, "allscripts.txt");
-            var imageDbManifestPath = Path.Combine(args.ScriptDirectory, "BuildImageDatabase.sql");
-            var migrationDirectoryPath = Path.Combine(args.ScriptDirectory, "migration");
-            var schemaDirectoryPath = Path.Combine(args.ScriptDirectory, "schema");
-            var seedDirectoryPath = Path.Combine(args.ScriptDirectory, "seed");
-            var cmsDatabaseName = $"CMS_{args.DatabaseName}";
-            var imageDatabaseName = $"CMSi_{args.DatabaseName}";
+            var hostName = args.DatabaseName;
+            var sqlScriptsPath = args.ScriptDirectory + @"\";
+            var masterConnectionString = args.ConnectionString + "Initial Catalog=master;";
+            var imageConnectionString = args.ConnectionString + "Initial Catalog=CMSi_" + args.DatabaseName + ";";
+            var elmahConnectionString = args.ConnectionString + "Initial Catalog=elmah;";
+            var standardConnectionString = args.ConnectionString + "Initial Catalog=CMS_" + args.DatabaseName + ";";
 
-            var scriptFiles = new List<string>(File.ReadLines(cmsDbManifestPath));
-            scriptFiles.AddRange(Directory.EnumerateFiles(seedDirectoryPath));
-            scriptFiles.AddRange(Directory.EnumerateFiles(migrationDirectoryPath));
-
-            var queryBuilder = new StringBuilder();
-
-            queryBuilder.AppendLine($"create database {cmsDatabaseName}");
-            queryBuilder.AppendLine("go");
-            queryBuilder.AppendLine($"create database {imageDatabaseName}");
-            queryBuilder.AppendLine("go");
-            queryBuilder.AppendLine($"use {cmsDatabaseName}");
-            queryBuilder.AppendLine("go");
-
-            foreach (var file in scriptFiles)
-            {
-                var script = Path.Combine(schemaDirectoryPath, file);
-                var sql = File.ReadAllText(script);
-
-                queryBuilder.AppendLine(sql);
-                queryBuilder.AppendLine("go");
-            }
-
-            queryBuilder.AppendLine($"use {imageDatabaseName}");
-            queryBuilder.AppendLine("go");
-            queryBuilder.AppendLine(File.ReadAllText(imageDbManifestPath));
-            queryBuilder.AppendLine("go");
-
-            var sqlText = queryBuilder.ToString().Replace("ro-CMS_StarterDb", $"ro-{cmsDatabaseName}").Replace("p@ssw0rd", args.ReadonlyPassword);
-
-            using (var connection = new SqlConnection(args.ConnectionString))
-            {
-                connection.Open();
-
-                var cmsDbExists = connection.CreateCommand();
-                cmsDbExists.CommandText = $"select count(*) as dbcount from sys.databases where name = '{cmsDatabaseName}'";
-                var cmsDbExistsResult = (int)cmsDbExists.ExecuteScalar();
-
-                var imageDbExists = connection.CreateCommand();
-                imageDbExists.CommandText = $"select count(*) as dbcount from sys.databases where name = '{imageDatabaseName}'";
-                var imageDbExistsResult = (int)imageDbExists.ExecuteScalar();
-
-                if (cmsDbExistsResult > 0 || imageDbExistsResult > 0)
-                {
-                    connection.Close();
-                    return View("DbAlreadyExistsResult"); // TODO: actually add something useful to the results display
-                }
-
-                foreach (var sqlBatch in sqlText.Split(new[] { "\r\nGO\r\n", "\r\ngo\r\n", "\r\nGo\r\n", "\r\ngO\r\n" }, StringSplitOptions.RemoveEmptyEntries))
-                {
-                    using (var command = connection.CreateCommand())
-                    {
-                        command.CommandText = sqlBatch;
-                        command.CommandTimeout = 60;
-                        command.ExecuteNonQuery();
-                    }
-                }
-
-                connection.Close();
-            }
+            DbUtil.CreateDatabase(hostName, sqlScriptsPath, masterConnectionString, imageConnectionString, elmahConnectionString, standardConnectionString);
 
             return View("DbCreatedResult");
         }
