fc86bcb715830dab09a161ec0f97f030ae5d65b1
Austin Lippert <6955342+aternex@users.noreply.github.com>
2018-12-20 11:42:55 -0500

    figures

--- CmsWeb/Areas/Figures/Controllers/FiguresController.cs
+++ CmsWeb/Areas/Figures/Controllers/FiguresController.cs
@@ -1,6 +1,6 @@
 ﻿using CmsData;
-using CmsWeb.Areas.Figures.Models;
 using CmsWeb.Lifecycle;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
@@ -9,25 +9,41 @@ namespace CmsWeb.Areas.Figures.Controllers
 {
     public class FiguresController : CMSBaseController
     {
-        public static List<ProgModel> Programs;
+        public class LookupItem
+        {
+            public string ItemType { get; set; }
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
 
-        public static ProgViewModel pvm = new ProgViewModel();
+        public class FiguresLookupItems
+        {
+            public IEnumerable<LookupItem> Programs { get; set; }
+            public IEnumerable<LookupItem> Divisions { get; set; }
+            public IEnumerable<LookupItem> Organizations { get; set; }
+            public IEnumerable<LookupItem> ContributionFunds { get; set; }
+        }
 
-        public static DivViewModel dvm = new DivViewModel();
-
-        public static OrgViewModel ovm = new OrgViewModel();
-
-        public static FundViewModel fvm = new FundViewModel();
-
-        // GET: Figures/Figures
         public FiguresController(IRequestManager requestManager) : base(requestManager)
         {
         }
 
         public ActionResult Index()
         {
-            Progs();
-            return View();
+            var model = GetLookupItems();
+            return View(model);
+        }
+
+        public ActionResult RefineAttendanceView()
+        {
+            var model = GetLookupItems();
+            return View(model);
+        }
+
+        public ActionResult RefineFundView()
+        {
+            var model = GetLookupItems();
+            return View(model);
         }
 
         public ActionResult GetMapData(int? progId)
@@ -36,6 +52,7 @@ namespace CmsWeb.Areas.Figures.Controllers
             return Json(test.GetChartData(progId).ToList(), JsonRequestBehavior.AllowGet);
         }
 
+        [ChildActionOnly]
         public ActionResult ChartDisplayView(int[] fundIdsArr)
         {
             var test = new GoogleChartsData();
@@ -43,136 +60,48 @@ namespace CmsWeb.Areas.Figures.Controllers
             return View(temp);
         }
 
-
+        [ChildActionOnly]
         public ActionResult AttendanceChartDisplayView(int[] orgIdsArr)
         {
-            ;
             var test = new GoogleChartsData();
             var temp = test.GetAttendanceChartData(orgIdsArr).ToList();
             return View(temp);
         }
 
-        public ActionResult RefineAttendanceView()
+        private FiguresLookupItems GetLookupItems()
         {
-            Progs();
-            return View();
-        }
+            var cacheKey = $"{CurrentDatabase.Host}|FiguresLookupItems";
+            var cachedItems = (FiguresLookupItems)CurrentHttpContext.Cache.Get(cacheKey);
 
-        public ActionResult RefineFundView()
-        {
-            return View();
-        }
+            if (cachedItems != null)
+            {
+                return cachedItems;
+            }
 
-        public List<FundModel> Funds()
-        {
-            var fundList = (from f in CurrentDatabase.ContributionFunds
-                            select new FundModel
-                            {
-                                Id = f.FundId,
-                                FundName = f.FundName
-                            }).ToList();
-
-            return fundList;
-        }
-
-        public static void Progs()
-        {
-            var pList = (from f in DbUtil.Db.Programs
-                         select new ProgModel
+            var items = (from f in CurrentDatabase.Programs
+                         join d in CurrentDatabase.Divisions on f.Id equals d.ProgId
+                         join o in CurrentDatabase.Organizations on d.Id equals o.DivisionId
+                         select new
                          {
-                             Id = f.Id,
-                             ProgName = f.Name,
-                             DivList = Divisions(f.Id)
+                             ProgramId = f.Id,
+                             ProgramName = f.Name,
+                             DivisionId = d.Id,
+                             DivisionName = d.Name,
+                             OrganizationId = o.OrganizationId,
+                             OrganizationName = o.OrganizationName
                          }).ToList();
-            Programs = pList;
-        }
 
-        public static List<DivModel> Divisions(int progId)
-        {
-            var divisionList = (from f in DbUtil.Db.Divisions
-                                where f.ProgId == progId
-                                select new DivModel
-                                {
-                                    Id = f.Id,
-                                    DivName = f.Name,
-                                    OrgList = Organizations(f.Id)
-                                }).ToList();
-
-            return divisionList;
-        }
-
-        public static List<OrgModel> Organizations(int divId)
-        {
-            var orgList = (from f in DbUtil.Db.Organizations
-                           where f.DivisionId == divId
-                           select new OrgModel
-                           {
-                               Id = f.OrganizationId,
-                               OrgName = f.OrganizationName
-                           }).ToList();
-
-            return orgList;
-        }
-
-        public ActionResult ProgramView()
-        {
-            pvm.Proglist.Clear();
-
-            foreach (var pd in Programs)
+            cachedItems = new FiguresLookupItems
             {
-                pvm.Proglist.Add(pd);
-            }
+                Programs = items.Select(x => new LookupItem { Id = x.ProgramId, Name = x.ProgramName }).Distinct().ToList(),
+                Divisions = items.Select(x => new LookupItem { Id = x.DivisionId, Name = x.DivisionName }).Distinct().ToList(),
+                Organizations = items.Select(x => new LookupItem { Id = x.OrganizationId, Name = x.OrganizationName }).Distinct().ToList(),
+                ContributionFunds = CurrentDatabase.ContributionFunds.Select(x => new LookupItem { Id = x.FundId, Name = x.FundName }).ToList()
+            };
 
-            return View(pvm);
-        }
+            CurrentHttpContext.Cache.Insert(cacheKey, cachedItems, null, DateTime.UtcNow.AddHours(1), TimeSpan.FromMinutes(5));
 
-        public ActionResult DivisionView(int? progId)
-        {
-            dvm.Divlist.Clear();
-            if (progId != null)
-            {
-                var cd = Programs.Find(p => p.Id == progId);
-
-                foreach (var spd in cd.DivList)
-                {
-                    dvm.Divlist.Add(spd);
-                }
-            }
-
-            return View(dvm);
-        }
-
-        public ActionResult OrganizationView(int? progId, int? divId)
-        {
-            ovm.Orglist.Clear();
-            if (progId == null || divId == null)
-            {
-                return View(ovm);
-            }
-
-            var cd = Programs.Find(p => p.Id == progId);
-            var spd = cd.DivList.Find(p => p.Id == divId);
-
-            foreach (var cpd in spd.OrgList)
-            {
-                ovm.Orglist.Add(cpd);
-            }
-
-            return View(ovm);
-        }
-
-        public ActionResult FundView()
-        {
-            fvm.Fundlist.Clear();
-
-            fvm.Fundlist = (from f in CurrentDatabase.ContributionFunds
-                            select new FundModel
-                            {
-                                Id = f.FundId,
-                                FundName = f.FundName
-                            }).ToList();
-
-            return View(fvm);
+            return cachedItems;
         }
     }
 }
