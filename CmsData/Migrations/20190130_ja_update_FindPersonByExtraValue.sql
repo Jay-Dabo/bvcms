1b8dba66e692fc492d7925130d7634f13e334c87
Jose Armando Hernandez Flores <josearmanhf@gmail.com>
2019-01-30 14:24:00 -0600

    Update FindPersonByExtraValue sql function.

---
+++ CmsData/Migrations/20190130_ja_update_FindPersonByExtraValue.sql
@@ -0,0 +1,26 @@
+﻿IF EXISTS (SELECT * FROM sysobjects WHERE id = object_id(N'[dbo].[FindPersonByExtraValue]') AND type in (N'FN', N'IF',N'TF', N'FS', N'FT'))
+BEGIN	
+	DROP FUNCTION [dbo].[FindPersonByExtraValue]	
+END
+GO
+
+CREATE FUNCTION [dbo].[FindPersonByExtraValue](@key nvarchar(100), @value nvarchar(max))
+RETURNS @t TABLE ( PeopleId INT)
+AS
+BEGIN
+
+	INSERT INTO @t
+	SELECT PeopleId 
+	FROM dbo.PeopleExtra
+	WHERE Field = @Key
+	AND ( StrValue = @Value
+        OR DateValue = dbo.ParseDate( @Value)
+        OR Data = @Value
+        OR IntValue = @Value
+        OR IntValue2 = @Value
+        OR BitValue = @Value
+        OR FieldValue = @Value )
+	
+	RETURN
+END
+GO
